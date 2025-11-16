edad398ce897558dc06041291d99692c37fed221
Steven De George SR <ehomecreations@gmail.com>
2025-11-16 18:11:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HDir
+HotDir program for the Commander X16 in nxtBasic
---
+++ c.bat
@@ -0,0 +1,5 @@
+C:\8bitProgramming\nxtBasic\Download\nxtBasic-v1.0-win\nxtBasic.exe h.bas
+pause
+del h
+ren h.prg H
+x16noCard1x.bat
